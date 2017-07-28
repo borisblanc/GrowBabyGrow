6f64523d61b2f17d35be5de2677dd15d9e504a1d
DESKTOP-26JDFKC\boris <borisblanc@gmail.com>
2017-07-28 01:31:49 -0400

    delete function

--- app/src/main/java/com/app/growbabygrow/MainMenuActivity.java
+++ app/src/main/java/com/app/growbabygrow/MainMenuActivity.java
@@ -1,19 +1,20 @@
 package com.app.growbabygrow;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import static com.app.growbabygrow.R.id.fab;
 
@@ -26,6 +27,7 @@ public class MainMenuActivity extends AppCompatActivity {
     private Boolean savedexists;
     private Boolean startbabygrow;
     private Spinner timeperiods;
+    private ImageButton delete;
 
     private Context context;
 
@@ -40,19 +42,22 @@ public class MainMenuActivity extends AppCompatActivity {
         timeperiods = (Spinner) findViewById(R.id.spinnerTime);
         fabnewproj = (FloatingActionButton) findViewById(fab);
         tvnewproject = (TextView) findViewById(R.id.TV_noProjects);
+        delete = (ImageButton) findViewById((R.id.deletebtn));
 
-        sharedpreferences = getSharedPreferences(getString(R.string.preference_file_key1), Context.MODE_PRIVATE);
-        String name = sharedpreferences.getString(getString(R.string.saved_name), null);
-        String period = sharedpreferences.getString(getString(R.string.saved_period), null);
+        //Default first project will always use this constant R.string.preference_file_key1
+        sharedpreferences = getSharedPreferences(getString(R.string.p_file1_key), Context.MODE_PRIVATE);
+        String name = sharedpreferences.getString(getString(R.string.p_file1_saved_name), null);
+        String period = sharedpreferences.getString(getString(R.string.p_file1_saved_period), null);
 
         if (name == null) {//no projects exist
+            ShowBabyGrowNew();
             savedexists = false;
             startbabygrow = false;
         }
         else {
             savedexists = true;
             startbabygrow = true;
-            SetupBabyGrow(name, period);
+            ShowBabyGrowReady(name, period);
         }
 
         fabnewproj.setOnClickListener(new View.OnClickListener() {
@@ -60,10 +65,7 @@ public class MainMenuActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (!startbabygrow) {
                     if (!savedexists) {
-                        tvnewproject.setText(R.string.save_project_settings);
-                        fabnewproj.setImageResource(android.R.drawable.ic_menu_save);
-                        txtname.setVisibility(View.VISIBLE);
-                        timeperiods.setVisibility(View.VISIBLE);
+                        ShowBabyGrowInput();
                         savedexists = true;
                     }
                     else {
@@ -71,12 +73,12 @@ public class MainMenuActivity extends AppCompatActivity {
                             try
                             {
                                 SharedPreferences.Editor editor = sharedpreferences.edit();
-                                editor.putString(getString(R.string.saved_name), txtname.getText().toString());
-                                editor.putString(getString(R.string.saved_period), timeperiods.getSelectedItem().toString());
+                                editor.putString(getString(R.string.p_file1_saved_name), txtname.getText().toString());
+                                editor.putString(getString(R.string.p_file1_saved_period), timeperiods.getSelectedItem().toString());
                                 editor.apply();
 
                                 startbabygrow = true;
-                                SetupBabyGrow(txtname.getText().toString(), timeperiods.getSelectedItem().toString());
+                                ShowBabyGrowReady(txtname.getText().toString(), timeperiods.getSelectedItem().toString());
 
                             } catch (Exception ex) {
                                 Log.d(TAG, ex.getMessage());
@@ -96,10 +98,51 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+        delete.setOnClickListener(new View.OnClickListener() {
+
+              @Override
+              public void onClick(View v) {
+                  new AlertDialog.Builder(MainMenuActivity.this)
+                          .setTitle("!Delete Baby Grow Session!")
+                          .setMessage("Are you sure you want to delete all videos and info on your Baby Grow?")
+                          .setIcon(android.R.drawable.ic_dialog_alert)
+                          .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                              public void onClick(DialogInterface dialog, int whichButton) {
+                                  ClearSharedDefault();
+                                  //todo add logic to delete actual videos
+                                  ShowBabyGrowNew();
+                                  savedexists = false;
+                                  startbabygrow = false;
+                              }})
+                          .setNegativeButton(android.R.string.no, null).show();
+              }
+        });
+
 
     }
 
-    private void SetupBabyGrow(String name, String period) {
+    private void ShowBabyGrowNew()
+    {
+        tvnewproject.setText(R.string.add_new_project);
+        fabnewproj.setImageResource(android.R.drawable.ic_input_add);
+        timeperiods.setVisibility(View.INVISIBLE);
+        txtname.setVisibility(View.INVISIBLE);
+        delete.setVisibility(View.INVISIBLE);
+    }
+
+    private void ShowBabyGrowInput()
+    {
+        tvnewproject.setText(R.string.save_project_settings);
+        fabnewproj.setImageResource(android.R.drawable.ic_menu_save);
+        txtname.setVisibility(View.VISIBLE);
+        txtname.setEnabled(true);
+        timeperiods.setVisibility(View.VISIBLE);
+        timeperiods.setEnabled(true);
+        delete.setVisibility(View.INVISIBLE);
+    }
+
+    private void ShowBabyGrowReady(String name, String period) {
         tvnewproject.setText(R.string.go_capture_start);
         fabnewproj.setImageResource(android.R.drawable.star_big_on);
         txtname.setVisibility(View.VISIBLE);
@@ -108,6 +151,7 @@ public class MainMenuActivity extends AppCompatActivity {
         timeperiods.setVisibility(View.VISIBLE);
         timeperiods.setSelection(getSpinnerIndex(timeperiods, period));
         timeperiods.setEnabled(false);
+        delete.setVisibility(View.VISIBLE);
     }
 
     private int getSpinnerIndex(Spinner spinner, String myString) {
@@ -122,12 +166,12 @@ public class MainMenuActivity extends AppCompatActivity {
         return index;
     }
 
-    private void Clearshared() {
-
-        SharedPreferences preferences = getSharedPreferences(getString(R.string.preference_file_key1), Context.MODE_PRIVATE);
+    private void ClearSharedDefault() {
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.p_file1_key), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.clear();
         editor.apply();
+        Toast.makeText(MainMenuActivity.this, "Everything has been deleted", Toast.LENGTH_SHORT).show();
     }
 
 
